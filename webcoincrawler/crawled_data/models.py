8fc7d9a7e147c3bcbacffe6b79aa487b7598b388
mooyoung2309 <mooyoung2309@gmail.com>
2021-03-16 17:27:58 +0900

    파일 정리

--- webcoincrawler/crawled_data/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-# Create your models here.
-class BoardData(models.Model):
-    title = models.CharField(max_length=300)
-    detail = models.CharField(max_length=10000)
-    coin_name=models.CharField(max_length=30)
-    added_date=models.DateTimeField()
-    gn_date=models.DateTimeField()
