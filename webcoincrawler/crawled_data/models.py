7f2877574ed712f1d4883121f08fdc21a45e98f6
Y-Joo <wldudwn99@gmail.com>
2021-04-02 14:15:27 +0900

    no message

--- webcoincrawler/crawled_data/models.py
+++ webcoincrawler/crawled_data/models.py
@@ -1,2 +1,9 @@
 from django.db import models
-# Create your models here.
+
+
+class BlogData(models.Model):
+    title = models.CharField(max_length=200)
+    link = models.URLField()
+
+    def __str__(self):
+       return self.title
--- webcoincrawler/crawled_data/views.py
+++ webcoincrawler/crawled_data/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
-def coin_list(request):
-    return render(request, 'crawled_data/coin_calender.html', {})
+from .models import BlogData
+def BlogData_list(request):
+    context = {
+        "BlogDatas": BlogData.objects.all()
+    }
+    return render(request, 'crawled_data/coin_calender.html', context)
 # Create your views here.
