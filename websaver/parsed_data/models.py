e14b3ed4d4b322ee87991926839a2a10b08f4a36
mooyoung2309 <mooyoung2309@gmail.com>
2021-04-02 14:22:51 +0900

    Django models에서 jsonfield사용

--- websaver/parsed_data/models.py
+++ websaver/parsed_data/models.py
@@ -3,4 +3,7 @@ from django.db import models
 
 class BlogData(models.Model):
     title = models.CharField(max_length=200)
-    link = models.URLField()
+    content = models.JSONField(default=dict)
+
+    def __str__(self):
+       return self.title
