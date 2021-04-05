ea79811ebd4138eb2b1bc2a1c4e1d50b6043d770
Y-Joo <wldudwn99@gmail.com>
2021-04-05 22:51:07 +0900

    no message

--- webcoincrawler/config/settings.py
+++ webcoincrawler/config/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django_crontab',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
