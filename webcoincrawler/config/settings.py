11293764400796e6217a204e878f208ff3ab0582
Y-Joo <wldudwn99@gmail.com>
2021-04-06 17:26:48 +0900

    no message

--- webcoincrawler/config/settings.py
+++ webcoincrawler/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^5g&99z#s48#8jnd=edgnpxj(50dnadm%n65h!eka0knc=xt*p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['54.180.104.57', '127.0.0.1']
 
