955d5e60d62d743ada676a454f26bace19154f31
Y-Joo <wldudwn99@gmail.com>
2021-04-06 17:20:03 +0900

    no message

--- webcoincrawler/config/settings.py
+++ webcoincrawler/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^5g&99z#s48#8jnd=edgnpxj(50dnadm%n65h!eka0knc=xt*p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['54.180.104.57']
+ALLOWED_HOSTS = ['54.180.104.57', '127.0.0.1']
 
 
 # Application definition
@@ -119,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '..', '.static_root')
+#STATIC_ROOT = os.path.join(BASE_DIR, '..', '.static_root')
