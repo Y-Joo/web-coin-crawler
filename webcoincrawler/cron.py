442df4c10642b6ca64d4a3f26e33e1d828d7183c
Y-Joo <wldudwn99@gmail.com>
2021-04-08 16:48:51 +0900

    no message

--- webcoincrawler/cron.py
+++ webcoincrawler/cron.py
@@ -33,6 +33,6 @@ def crontab():
     urls = coinscalendar.get_urls()
     for url in urls:
         coinscalendar.do_crawl(url, result)
-
+    BlogData.objects.all().delete()
     BlogData(title="COIN_DATA", content=json.dumps(preprocessingDict(result.copy()))).save()
 crontab()
