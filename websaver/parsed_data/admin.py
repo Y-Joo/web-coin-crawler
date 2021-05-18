07e6ae4d08c0e7707e10df734b6ccbe9bbd626c5
mooyoung2309 <mooyoung2309@gmail.com>
2021-05-18 15:56:36 +0900

    브랜치 초기화

--- websaver/parsed_data/admin.py
+++
@@ -1,7 +0,0 @@
-# parsed_data/admin.py
-from django.contrib import admin
-# models에서 BlogData를 import 해옵니다.
-from .models import BlogData
-
-# 아래의 코드를 입력하면 BlogData를 admin 페이지에서 관리할 수 있습니다.
-admin.site.register(BlogData)
--- websaver/parsed_data/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class BlogData(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.JSONField(default=dict)
-
-    def __str__(self):
-       return self.title
