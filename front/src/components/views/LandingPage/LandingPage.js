1a228a9eb93138575033b1f4573c028ee842a31e
mooyoung2309 <mooyoung2309@gmail.com>
2021-05-20 13:15:48 +0900

    calendar 크기 맞추기

--- front/src/components/views/LandingPage/LandingPage.js
+++ front/src/components/views/LandingPage/LandingPage.js
@@ -1,19 +1,71 @@
-import React from 'react'
-import { Row, Col } from 'antd';
+import React from 'react';
+import { Calendar, Badge } from 'antd';
+import './LandingPage.css'
+
+function getListData(value) {
+  let listData;
+  switch (value.date()) {
+    case 8:
+      listData = [
+        { type: 'warning', content: 'This is warning event.' },
+        { type: 'success', content: 'This is usual event.' },
+      ];
+      break;
+    case 10:
+      listData = [
+        { type: 'warning', content: 'This is warning event.' },
+        { type: 'success', content: 'This is usual event.' },
+        { type: 'error', content: 'This is error event.' },
+      ];
+      break;
+    case 15:
+      listData = [
+        { type: 'warning', content: 'This is warning event' },
+        { type: 'success', content: 'This is very long usual event。。....' },
+        { type: 'error', content: 'This is error event 1.' },
+        { type: 'error', content: 'This is error event 2.' },
+        { type: 'error', content: 'This is error event 3.' },
+        { type: 'error', content: 'This is error event 4.' },
+      ];
+      break;
+    default:
+  }
+  return listData || [];
+}
+
+function dateCellRender(value) {
+  const listData = getListData(value);
+  return (
+    <ul className="events">
+      {listData.map(item => (
+        <li key={item.content}>
+          <span>10</span>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function getMonthData(value) {
+  if (value.month() === 8) {
+    return 1394;
+  }
+}
+
+function monthCellRender(value) {
+  const num = getMonthData(value);
+  return num ? (
+    <div className="notes-month">
+      <section>{num}</section>
+      <span>Backlog number</span>
+    </div>
+  ) : null;
+}
 
 function LandingPage() {
     return (
-        <Row>
-        <Col xs={24} xl={8}>
-          One of three columns
-        </Col>
-        <Col xs={24} xl={8}>
-          One of three columns
-        </Col>
-        <Col xs={24} xl={8}>
-          One of three columns
-        </Col>
-      </Row>
+      <Calendar dateCellRender={dateCellRender} monthCellRender={monthCellRender} />
+
     )
 }
 
